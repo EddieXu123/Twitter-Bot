a361334187e8372d79adf7b61105cf46e228ba36
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-05-22 22:58:28 -0400

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,7 @@
+# Keys that control the Bot in developer mode
+# Go to developer.twitter.com/en/apps, create an app and your keys will appear
+api_key = 'YAioKhD86e4QkefSXMZmzi8tv'
+api_secret_key = 'B1MP0Wi2c0lFv67ZMKpO32ah3FxmzbyZM9gPr3LfgWIrNSFb9k'
+access_token = '1262246238595903494-qKAfYWJD9s8RVxbciIeBbAzvWI4aIH'
+access_token_secret = '42oELaFsOWyp6K61rF6l3wUnDoT6xYAVJALLtLhNsfwWH'
+news_api_key = 'a391df2bb6e34bb5a9233bb52e5e8041'
